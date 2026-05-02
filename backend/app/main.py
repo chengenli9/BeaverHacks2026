6552ce293f564f8dbb3313fa3f0dab5cba256cf8
Gisnseng <96270867+Gisnseng@users.noreply.github.com>
2026-05-02 14:32:14 -0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,10 @@
-//Test
+from fastapi import FastAPI
+from app.api import jobs, projects, generate, render, critique
+
+app = FastAPI()
+
+app.include_router(jobs.router)
+app.include_router(projects.router)
+app.include_router(generate.router)
+app.include_router(render.router)
+app.include_router(critique.router)
